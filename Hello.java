b6af2c976e9fdaff629c6cdaac3fc68d1bc905d3
Draxler-savage <prospermuogbo16@gmail.com>
2024-06-25 12:58:12 -0700

    First Commit

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    public static void main(String[] args) {
+
+        boolean b = true;
+        System.out.println(b);
+        // char c ='a'
+        //float num = 12.3f;
+    char x1 ='\u0032';
+    System.out.println(x1);
+    }
